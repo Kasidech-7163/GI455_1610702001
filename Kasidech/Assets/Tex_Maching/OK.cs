844a626ab95fbe280173d571fd42c76452c51ef2
DESKTOP-LVN60EJ\computer <kasidech.puen@bumail.net>
2021-01-28 15:26:38 +0700

    homework_Week_1

---
+++ Kasidech/Assets/Tex_Maching/OK.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OK : MonoBehaviour
+{
+    public string ShowText;
+    public GameObject Random;
+    public GameObject Show;
+
+
+
+    public void Showname()
+    {
+        ShowText = Random.GetComponent<Text>().text;
+        Show.GetComponent<Text>().text = ShowText + "ฮัลโหลตัวเองง";
+    }
+}
